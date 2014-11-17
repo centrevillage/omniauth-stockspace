cd4189e9649912a03b8675c123cc19d67139e812
centrevillage <centrevillage@gmail.com>
2014-11-17 13:17:19 +0900

    change oauth 1 to 2

--- lib/omniauth/strategies/stockspace.rb
+++ lib/omniauth/strategies/stockspace.rb
@@ -1,17 +1,21 @@
-require 'omniauth-oauth'
-require 'multi_json'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class Stockspace < OmniAuth::Strategies::OAuth
+    class Stockspace < OmniAuth::Strategies::OAuth2
       option :name, 'stockspace'
 
       option :client_options, {
         :site => 'http://localhost:3001',
-        :authorize_path => '/oauth/authorize',
-        :access_token_path => '/oauth/revoke',
-        :request_token_path => '/oauth/token'
+        :authorize_url => 'https://localhost:3001/oauth/authorize',
+        :token_url => 'http://localhost:3001/oauth/token'
       }
+
+      def request_phase
+        options[:scope] ||= 'basic'
+        options[:response_type] ||= 'code'
+        super
+      end
 
       uid { raw_info['_id'] }
 
